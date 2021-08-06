60ff80d2ff23d799d7e2138c77c56edeb877efa5
danifosy <mail@daniha.de>
2021-08-06 15:00:19 +0200

    💄 Add basic styling in components

--- src/components/villagers.ts
+++ src/components/villagers.ts
@@ -1,41 +1,51 @@
 import { createElement } from '../../src/utils/createElement';
+import styles from './villagers.module.css';
 
-function createVillagerCard(): HTMLElement {
+export function createVillagerCard(): HTMLElement {
   return createElement('div', {
-    className: 'villagerCard',
+    className: styles.card,
     /*Whole card with child article*/
     childElements: [
       createElement('article', {
-        className: 'villagerCard',
-        /*img child of article*/
+        className: styles.article,
 
+        /*img child of article*/
         childElements: [
           createElement('img', {
-            className: 'villagerImg',
+            className: styles.img,
             src: 'https://acnhapi.com/v1/images/villagers/1',
           }),
           /*new div for villager infos*/
 
           createElement('div', {
-            className: 'villagerInfo',
+            className: styles.info,
 
             /*children with infos (the html elements) */
             childElements: [
               createElement('h1', {
-                innerHTML: 'Cyrano',
-                className: 'Name',
+                innerText: 'Cyrano',
+                className: styles.name,
               }),
               /*birthday */
               createElement('p', {
-                innerHTML: 'Birthday',
-                className: 'birthday',
+                className: styles.birthday,
+                innerText: 'March 9th',
               }),
-
               /*Species */
-              createElement('p', {}),
-
+              createElement('p', {
+                innerText: 'Anteater',
+                className: styles.species,
+              }),
               /*Gender */
-              createElement('p', {}),
+              createElement('p', {
+                innerText: 'Male',
+                className: styles.gender,
+              }),
+              /*catch-phrase */
+              createElement('p', {
+                innerText: '"ah-CHOO"',
+                className: styles.catchphrase,
+              }),
             ],
           }),
         ],
